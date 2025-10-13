4a6e1b1de40fe29677ab3accca25b816ff792f8d
Dominik Schrempf <dominik.schrempf@gmail.com>
2025-10-13 09:45:05 +0200

    pcap-client: move to flake parts; remove mermaid dep (large)

--- flake.nix
+++ flake.nix
@@ -1,24 +1,35 @@
 {
   description = "`hs-bindgen` Nix tutorial";
 
-  inputs.flake-utils.url = "github:numtide/flake-utils";
-
-  inputs.pcap-client.url = "path:./pcap-client";
-
-  inputs.pcap-th.url = "path:./pcap-th";
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    pcap-client.url = "path:./pcap-client";
+    pcap-th.url = "path:./pcap-th";
+  };
 
   outputs =
-    {
-      self,
-      flake-utils,
+    inputs@{
+      flake-parts,
       pcap-client,
       pcap-th,
+      ...
     }:
-    flake-utils.lib.eachDefaultSystem (system: {
-      checks = {
-        hs-bindgen-cli-check = pcap-client.packages.${system}.hs-bindgen-cli;
-        pcap-client-check = pcap-client.packages.${system}.pcap-client;
-        pcap-th-check = pcap-th.packages.${system}.pcap-th;
-      };
-    });
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [ ];
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
+      perSystem =
+        { system, ... }:
+        {
+          checks = {
+            hs-bindgen-cli-check = pcap-client.packages.${system}.hs-bindgen-cli;
+            pcap-client-check = pcap-client.packages.${system}.pcap-client;
+            pcap-th-check = pcap-th.packages.${system}.pcap-th;
+          };
+        };
+    };
 }
--- pcap-client/flake.nix
+++ pcap-client/flake.nix
@@ -1,70 +1,75 @@
 {
   description = "`hs-bindgen` tutorial: Client project";
 
-  inputs.flake-utils.url = "github:numtide/flake-utils";
-
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-
-  inputs.hs-bindgen.url = "github:dschrempf/hs-bindgen-flake";
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    hs-bindgen.url = "github:dschrempf/hs-bindgen-flake";
+  };
 
   outputs =
-    {
-      self,
-      flake-utils,
+    inputs@{
+      flake-parts,
       nixpkgs,
       hs-bindgen,
+      ...
     }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [ hs-bindgen.overlays.default ];
-        };
-        # TODO: I don't know why `mermaid-cli` triggers a Chromium rebuild using
-        # the `hs-bindgen` overlay, but here we go.
-        defPkgs = import nixpkgs {
-          inherit system;
-        };
-        hpkgs = pkgs.haskellPackages;
-        hlib = pkgs.haskell.lib.compose;
-        pcap-client = hlib.overrideCabal (drv: {
-          executableToolDepends = (drv.executableToolDepends or [ ]) ++ [
-            pkgs.hs-bindgen-cli
-            pkgs.hsBindgenHook
-          ];
-          postUnpack = ''
-            ${drv.postUnpack or ""}
-            (cd pcap-client; ${pkgs.bash}/bin/bash ${./generate-bindings})
-          '';
-        }) (hpkgs.callCabal2nix "pcap-client" ./. { });
-      in
-      {
-        packages = {
-          inherit pcap-client;
-          inherit (pkgs) hs-bindgen-cli;
-          default = pcap-client;
-        };
-        devShells = {
-          default = hpkgs.shellFor {
-            packages = _: [ pcap-client ];
-            nativeBuildInputs = [
-              # Haskell toolchain.
-              hpkgs.cabal-install
-              hpkgs.ghc
-              hpkgs.haskell-language-server
-
-              # `hs-bindgen` client.
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
+      perSystem =
+        {
+          pkgs,
+          system,
+          ...
+        }:
+        let
+          hpkgs = pkgs.haskellPackages;
+          hlib = pkgs.haskell.lib.compose;
+          pcap-client = hlib.overrideCabal (drv: {
+            executableToolDepends = (drv.executableToolDepends or [ ]) ++ [
               pkgs.hs-bindgen-cli
-
-              # Connect `hs-bindgen` to the Clang toolchain and `libpcap`.
               pkgs.hsBindgenHook
-
-              # Misc.
-              defPkgs.mermaid-cli
             ];
+            postUnpack = ''
+              ${drv.postUnpack or ""}
+              (cd pcap-client; ${pkgs.bash}/bin/bash ${./generate-bindings})
+            '';
+          }) (hpkgs.callCabal2nix "pcap-client" ./. { });
+        in
+        {
+          _module.args.pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ hs-bindgen.overlays.default ];
+          };
+
+          packages = {
+            inherit pcap-client;
+            inherit (pkgs) hs-bindgen-cli;
+            default = pcap-client;
+          };
+
+          devShells = {
+            default = hpkgs.shellFor {
+              packages = _: [ pcap-client ];
+              nativeBuildInputs = [
+                # Haskell toolchain.
+                hpkgs.cabal-install
+                hpkgs.ghc
+                hpkgs.haskell-language-server
+
+                # `hs-bindgen` client.
+                pkgs.hs-bindgen-cli
+
+                # Connect `hs-bindgen` to the Clang toolchain and `libpcap`.
+                pkgs.hsBindgenHook
+              ];
+            };
           };
         };
-      }
-    );
+    };
 }
